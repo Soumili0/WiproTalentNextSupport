89c93e16e63189ab0e81983c585477d500063fef
Soumili Samanta <soumili.samanta18@gmail.com>
2026-07-29 10:28:10 +0530

    Add files via upload

---
+++ WelcomeMessage.java
@@ -0,0 +1,10 @@
+public class WelcomeMessage {
+   public static void main(String[]args){
+    if(args.length>0){
+        System.out.println("welcome"+" "+args[0]);
+    }
+    else{
+        System.out.println("give comment");
+    }
+   } 
+}
